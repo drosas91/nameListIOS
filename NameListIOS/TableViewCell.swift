17a808f0fba429d597049bcad850ca80c6507f96
Danilo Rosas <drosasarevalo@gmail.com>
2019-07-10 12:18:21 -0400

    List of names in the TableView

---
+++ NameListIOS/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  NameListIOS
+//
+//  Created by Danilo Rosas Arévalo on 7/10/19.
+//  Copyright © 2019 Danilo Rosas Arévalo. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- NameListIOS/ViewController.swift
+++ NameListIOS/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var newTable: UITableView!
+    
+    var array = ["Name01", "Name02", "Name03", "Name04"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newTable.delegate = self
+        newTable.dataSource = self
+        
     }
 
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let names = array[indexPath.row]
+        let cell = newTable.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+    
+        cell.nameLabel.text = names
+        
+        return cell
+    }
+    
+}
