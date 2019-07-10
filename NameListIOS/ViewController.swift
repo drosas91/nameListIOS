081dc518b9463e01d1459122b0170f8ccfe9032a
Danilo Rosas <drosasarevalo@gmail.com>
2019-07-10 10:33:39 -0400

    Initial files of project

---
+++ NameListIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NameListIOS
+//
+//  Created by Danilo Rosas Arévalo on 7/10/19.
+//  Copyright © 2019 Danilo Rosas Arévalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
